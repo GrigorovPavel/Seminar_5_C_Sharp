c092f10f0a318dcf6a499f505b02d2d3012b8d41
Pavel <grigorov_ps@mail.ru>
2023-10-22 18:22:11 +0300

    Создали проект Task32 (Заменяет положительные элементМассив  на отрицательные и наоборот)

---
+++ Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 32
+// Напишите программу замены элементов массива:
+// положительные элементы заменить на отрицательные и наоборот
+// [-4,-8,8,2] -> [4,8,-8,-2]
+//
+int[] CreateArrayRndInt(int size, int max, int min) // Создаем Метод для Массива с переменными size, max, min 
+{
+    int[] arr = new int[size]; 
+    Random rnd = new Random(); 
+
+    for (int i = 0; i < size; i++) 
+    {
+        arr[i] = rnd.Next(min, max + 1); 
+    }
+    return arr; 
+}
+
+void PriintArray(int[] arr)
+{
+    Console.Write("["); 
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, "); 
+    }
+    Console.WriteLine("]"); 
+}
+
+void InversArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] *= -1;
+    }
+}
+
+int[] array = CreateArrayRndInt(12, 9, -9); 
+PriintArray(array); 
+
+InversArray(array);
+PriintArray(array); 
+
